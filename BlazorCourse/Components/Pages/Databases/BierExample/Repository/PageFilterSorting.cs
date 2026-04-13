1b69ad9c98f9798cbd241c264372d1f30638b607
jorislops <joris.lops@nhlstenden.com>
2026-04-13 08:54:36 +0200

    fixed db example with pagedresult

--- BlazorCourse/Components/Pages/Databases/BierExample/Repository/PageFilterSorting.cs
+++ BlazorCourse/Components/Pages/Databases/BierExample/Repository/PageFilterSorting.cs
@@ -11,8 +11,4 @@ public class PageFilterSorting
     
     public string OrderBy { get; set; } = nameof(Beer.Name);
     public string OrderDirection { get; set; } = "ASC";
-    public int CurrentPage { get; set; } = 1;
-    public int PageSize { get; set; } = 10;
-
-    public int Offset => (CurrentPage - 1) * PageSize;
 }
